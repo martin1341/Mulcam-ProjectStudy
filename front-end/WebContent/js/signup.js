fa91794fdab27ce2f58b1d6c26016bf2d50bc1ae
martin1341 <98645221+martin1341@users.noreply.github.com>
2022-02-23 19:18:35 +0900

    signup_220223

---
+++ front-end/WebContent/js/signup.js
@@ -0,0 +1,117 @@
+// 휴대폰번호 입력
+function changePhone1(){
+  const phone1 = document.getElementById("phone1").value // 010
+  if(phone1.length !== 3){
+    document.getElementById("phoneError").innerHTML = "핸드폰 번호가 올바르지 않습니다."
+  }
+  else{
+    document.getElementById("phone2").focus();
+  }
+}
+function changePhone2(){
+  const phone2 = document.getElementById("phone2").value // 1234
+  if(phone2.length !== 4){
+    document.getElementById("phoneError").innerHTML = "핸드폰 번호가 올바르지 않습니다."
+  }
+  else{
+    document.getElementById("phone3").focus();
+  }
+}
+function changePhone3(){
+  const phone3 = document.getElementById("phone3").value // 5678
+  if(phone3.length !== 4){
+    document.getElementById("phoneError").innerHTML = "핸드폰 번호가 올바르지 않습니다."
+  }
+  else{
+    document.getElementById("phoneError").innerHTML = ""
+  }
+}
+
+
+// 가입부분 체크
+function signUpCheck(){
+
+let email = document.getElementById("email").value
+let name = document.getElementById("name").value
+let password = document.getElementById("password").value
+let passwordCheck = document.getElementById("passwordCheck").value
+let area = document.getElementById("area").value
+let gender_man = document.getElementById("gender_man").checked
+let gender_woman = document.getElementById("gender_woman").checked
+let check = true;
+
+  // 이메일확인
+  if(email.includes('@')){
+    let emailId = email.split('@')[0]
+    let emailServer = email.split('@')[1]
+    if(emailId === "" || emailServer === ""){
+      document.getElementById("emailError").innerHTML="이메일이 올바르지 않습니다."
+      check = false
+    }
+    else{
+      document.getElementById("emailError").innerHTML=""
+    }
+  }else{
+    document.getElementById("emailError").innerHTML="이메일이 올바르지 않습니다."
+    check = false
+  }
+  
+// 이름확인
+if(name===""){
+  document.getElementById("nameError").innerHTML="이름이 올바르지 않습니다."
+  check = false
+}else{
+  document.getElementById("nameError").innerHTML=""
+}
+
+// 비밀번호 확인
+if(password !== passwordCheck){
+  document.getElementById("passwordError").innerHTML=""
+  document.getElementById("passwordCheckError").innerHTML="비밀번호가 동일하지 않습니다."
+  check = false
+}else{
+  document.getElementById("passwordError").innerHTML=""
+  document.getElementById("passwordCheckError").innerHTML=""
+}
+
+if(password===""){
+  document.getElementById("passwordError").innerHTML=""
+  check = false
+}
+if(passwordCheck===""){
+  document.getElementById("passwordCheckError").innerHTML=""
+  check = false
+}else{
+  document.getElementById("passwordCheckError").innerHTML="비밀번호가 일치하지 않습니다."
+}
+
+// 지역선택 확인
+if(area === "지역을 선택하세요."){
+  document.getElementById("areaError").innerHTML="지역을 선택해주세요."
+  check = false
+}else{
+  document.getElementById("areaError").innerHTML=""
+}
+
+
+// 성별체크확인
+if(!gender_man && !gender_woman){
+  document.getElementById("genderError").innerHTML="성별을 선택해주세요."
+  check = false
+}else{
+  document.getElementById("genderError").innerHTML=""
+}
+
+if(check){
+  document.getElementById("emailError").innerHTML=""
+  document.getElementById("nameError").innerHTML=""
+  document.getElementById("passwordError").innerHTML=""
+  document.getElementById("passwordCheckError").innerHTML=""
+  document.getElementById("areaError").innerHTML=""
+  document.getElementById("genderError").innerHTML=""
+
+  setTimeout(function() {
+    alert("가입이 완료되었습니다.") } ,0
+  );
+}
+}
