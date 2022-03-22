fa25904a7a081ff522e317a3df404e894610394e
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-03-22 15:39:40 +0900

    게시판 DTO 설정

---
+++ src/main/java/multi/kdt/seven/board/dto/ArticleDTO.java
@@ -0,0 +1,11 @@
+package multi.kdt.seven.board.dto;
+
+public class ArticleDTO {
+
+	int articleNumber;
+	String articleTitle;
+	String articleAuthor;
+	String articleContent;
+	String articleImage;
+	
+}
