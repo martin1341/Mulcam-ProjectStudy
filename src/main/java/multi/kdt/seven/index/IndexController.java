c8bd08ade9735e7e2669cd211a031e55234f4e7e
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-03-22 12:00:19 +0900

    index 구현, head/header/footer 분리

---
+++ src/main/java/multi/kdt/seven/index/IndexController.java
@@ -0,0 +1,14 @@
+package multi.kdt.seven.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+}
