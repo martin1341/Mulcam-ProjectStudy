cc7609f96c24bf3e21573a9c4099d70b839f9731
alfndp <alfndp25@gmail.com>
2022-03-23 16:17:36 +0900

    Member 작업

---
+++ src/main/java/multi/kdt/seven/login/dto/MemberDTO.java
@@ -0,0 +1,56 @@
+package multi.kdt.seven.login.dto;
+
+public class MemberDTO {
+	String id;
+	String pw;
+	String mwmbername;
+	String email;
+	String phone;
+	String area;
+	String gender;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getMwmbername() {
+		return mwmbername;
+	}
+	public void setMwmbername(String mwmbername) {
+		this.mwmbername = mwmbername;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+}
---
+++ src/main/java/multi/kdt/seven/login/service/MemberService.java
@@ -0,0 +1,8 @@
+package multi.kdt.seven.login.service;
+
+import multi.kdt.seven.login.dto.MemberDTO;
+
+public interface MemberService {
+	
+	public MemberDTO loginmember(MemberDTO dto);
+}
